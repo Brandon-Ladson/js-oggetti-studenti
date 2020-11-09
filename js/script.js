7ab23331ffb6cadb981b3e240d4fa505a16880f0
Brandon Ladson <brandonladson1999@gmail.com>
2020-11-09 13:30:37 +0100

    creazione regole js per il primo esercizio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 // Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
+
+var datiStudenti = {
+  "nome": "brandon",
+  "cognome": "ladson",
+  "eta": 21
+};
+
+// console.log(datiStudenti);
+
+for (var key in datiStudenti) {
+  console.log(key + ": " + datiStudenti[key]);
+}
